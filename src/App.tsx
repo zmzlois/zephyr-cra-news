ac52a472ed7fb5c20f00755ff1a1424131c44cfe
Lois <loisisar@outlook.com>
2024-01-13 14:45:00 +0000

    style: wtf tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="m-w-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-2xl mx-auto container py-12 px-6 border-red-400 border">
+     hi
+      </div>
+   </div>
   );
 }
 
